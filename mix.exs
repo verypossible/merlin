d8a79aaa135b5a9f5788230b62340493bd1f0c77
Daniel Spofford <daniel.spofford@verypossible.com>
2018-03-30 14:09:44 -0500

    Add hex package meta attributes

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Merlin.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,15 @@ defmodule Merlin.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      description: "A wise alchemist to guide your project's setup.",
+      maintainers: ["Very"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/verypossible/merlin"}
     ]
   end
 end
